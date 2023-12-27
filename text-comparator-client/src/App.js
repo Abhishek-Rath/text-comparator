b9fe5c2053aced4e88baaf2a4377f82fe2932251
Abhishek-Rath <abhi.rath39@gmail.com>
2023-12-27 20:17:11 +0530

    Modify UI

--- text-comparator-client/src/App.js
+++ text-comparator-client/src/App.js
@@ -44,56 +44,63 @@ const App = () => {
   }
 
   return (
-    <div className="container mx-auto p-4">
-      {/* Navbar */}
-      <nav className="bg-gray-800 p-4 text-center">
+    <>
+      <nav className="bg-gray-800 p-4 text-center mt-0">
         <div className="text-white font-bold text-xl">Text Comparator</div>
       </nav>
-      <div className="container items-center">
-        <button
-          className="mt-2 mx-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={compareTexts}
-        >
-          Compare
-        </button>
+      <div className="container mx-auto p-4">
+        {/* buttons */}
+        <div className="mt-2 text-center">
+          <button
+            className="mt-2 mx-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={compareTexts}
+          >
+            Compare
+          </button>
 
-        <button 
-          className=" mx-10 bg-gray-500 hover:bg-gray-70 text-white font-bold py-2 px-4 rounded"
-          onClick={handleClearText}  
-        >
-          Clear all
-        </button>
-      </div>
+          <button
+            className=" mx-10 bg-gray-500 hover:bg-gray-70 text-white font-bold py-2 px-4 rounded"
+            onClick={handleClearText}
+          >
+            Clear all
+          </button>
+        </div>
 
-
-      {/* Main Content */}
-      <div className="mt-4 flex">
-        <textarea
-          className="flex-1 h-64 border rounded p-2 mr-2"
-          value={text1}
-          onChange={handleTextChange1}
-          placeholder="Enter Text 1"
-        />
-        <textarea
-          className="flex-1 border rounded p-2 ml-2"
-          value={text2}
-          onChange={handleTextChange2}
-          placeholder="Enter Text 2"
-        />
-      </div>
-
-      {isCompared && differences && (
-        <div className="mt-4">
-          <h2 className="text-xl font-bold text-center">Differences:</h2>
-          <ReactDiffViewer
-            oldValue={text1}
-            newValue={text2}
-            splitView={true}
-            compareMethod={DiffMethod.CHARS}
+        {/* Main Content */}
+        <div className="mt-4 flex">
+          <textarea
+            className="flex-1 h-64 border rounded p-2 mr-2"
+            value={text1}
+            onChange={handleTextChange1}
+            placeholder="Enter Text 1"
+          />
+          <textarea
+            className="flex-1 border rounded p-2 ml-2"
+            value={text2}
+            onChange={handleTextChange2}
+            placeholder="Enter Text 2"
           />
         </div>
-      )}
-    </div>
+
+        {isCompared && differences && (
+          <div className="mt-4">
+            <h2 className="text-xl font-bold text-center">Differences:</h2>
+            <ReactDiffViewer
+              oldValue={text1}
+              newValue={text2}
+              splitView={true}
+              compareMethod={DiffMethod.CHARS}
+            />
+          </div>
+        )}
+
+      {/* Footer */}
+      <footer className="mt-8 text-center text-gray-500">
+        Made by Abhishek Rath
+      </footer>
+      </div>
+    </>
+
   );
 };
 
